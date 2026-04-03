926418e92e87f06a193eb31935d8c1641af43b75
YousefSalah360 <yousefsalah360@gmail.com>
2026-04-03 16:00:41 +0200

    feat: configure GitHub Pages deployment settings and base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server:  { port: 3000, open: true },
+  server: { port: 3000, open: true },
+  base: "/dashboard-project",
 });
